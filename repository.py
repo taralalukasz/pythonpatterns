5d5de6da7540c881c9da3c23b7beafe085f4df1b
taralalukasz <taralalukasz123@gmail.com>
2025-01-13 17:48:50 +0100

    chapter 2

---
+++ repository.py
@@ -0,0 +1,42 @@
+import abc #this stands for abstract base class
+from model.model import Batch
+
+
+#technically, every class witch has add() and get() methods is repository abstraction
+#duck typing - recognizing objects by it's methods  or behavior, not actual hardcoded Type (which in python frequently isn't defined)
+#ABC serves as interface in other languages - python doesn't have "interface" keyword, we have to simiulate it
+#CONCLUSION - Repository pattern is btter in bigger project. In small CRUD it's better to use automated ORM 
+class AbstractRepository(abc.ABC):
+
+    @abc.abstractmethod  #<- python will refuse to instantiate a class if all annotated methods are not implemented
+    def add(self, batch: Batch):
+        raise NotImplementedError
+    @abc.abstractmethod
+    def get(self, reference) -> Batch:
+        raise NotImplementedError
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(Batch).all()
+    
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches):
+        self._batches = batches
+
+    def add(self, batch):
+        self._batches.add(batch)
+
+    def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self):
+        return self._batches
---
+++ test_repository.py
@@ -0,0 +1,24 @@
+import model.model
+import repository
+
+
+
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_repository_can_save_a_batch(session):
+    batch = model.Batch("batch1", "RUSTY-SOAPDISH", 100, eta=None)
+
+    repo = repository.SqlAlchemyRepository(session)
+    repo.add(batch)  #under test
+    session.commit()  
+
+    rows = session.execute(  #verification
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+    )
+    assert list(rows) == [("batch1", "RUSTY-SOAPDISH", 100, None)]
