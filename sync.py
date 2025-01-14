695982422fe35dd3a2c8a37afa8a1487aeeaed2f
taralalukasz <taralalukasz123@gmail.com>
2025-01-14 17:41:11 +0100

    abstractions part1 done

---
+++ sync.py
@@ -0,0 +1,50 @@
+import hashlib
+import os
+import shutil
+from pathlib import Path
+
+
+BUF_SIZE = 65536 #size of one chunk of the file in bytes 
+def hash_file(path):
+    sha1 = hashlib.sha1()
+    with open(path, 'rb') as file:
+        chunk = file.read(BUF_SIZE)
+        while chunk:
+            sha1.update(chunk)
+            chunk = file.read(BUF_SIZE)
+    return sha1.hexdigest()        
+
+#sync source and destination
+def sync(source, dest):
+    #sync file exists , desc not
+    source_hashes = {}
+    for folder, _, files in os.walk(source):
+        for filename in files:
+            source_hashes[hash_file(Path(folder) / filename)]  = filename # this is the same as os.path.join(folder, filename)
+    
+    seen = {}
+    for folder, _, files in os.walk(dest):
+        for filename in files:
+            dest_file_path = Path(folder) / filename
+            dest_hash = hash_file(dest_file_path)
+            seen[dest_hash] = filename
+            # if not in sources, remove
+            if dest_hash not in source_hashes.keys():
+                dest_file_path.unlink()
+
+            #if hash equals but name doesn't - rename
+            elif dest_hash in source_hashes.keys() and filename != source_hashes[dest_hash]:
+                shutil.move(dest_file_path, Path(folder) / source_hashes[dest_hash])
+
+    # if not in destination, copy
+    for source_hash, source_filename in source_hashes.items():
+        if source_hash not in seen.keys():
+            shutil.copy(Path(source) / source_filename, Path(dest) /  source_filename)
+
+
+    # for key, value in source_hashes.items():
+    #     print(key)
+    #     print(value)
+
+
+sync("source", "destination")
---
+++ test_sync.py
@@ -0,0 +1,48 @@
+import tempfile
+import shutil
+import sync
+from pathlib import Path
+
+def test_when_a_file_exists_in_the_source_but_not_the_destination():
+    try:
+        source = tempfile.mkdtemp()
+        dest = tempfile.mkdtemp()
+        source_path = Path(source)
+        
+        content = "asdasdasdasd"
+        (source_path / "newfile.txt").write_text(content)
+        expected_path = Path(dest) / "newfile.txt"
+
+        sync.sync(source, dest)
+
+        assert expected_path.exists()
+        assert expected_path.read_text() == content
+
+    finally:
+        shutil.rmtree(source)
+        shutil.rmtree(dest)
+
+
+def test_when_a_file_has_been_renamed_in_the_source():
+    try:
+        source = tempfile.mkdtemp()
+        dest = tempfile.mkdtemp()
+
+        content = "fghfghfghfghfg"
+        file_to_delete = (Path(dest) / "newfile.txt")
+        (Path(dest) / "newfile.txt").write_text(content)
+
+        sync.sync(source, dest)
+
+        assert not file_to_delete.exists()
+
+    finally:
+        shutil.rmtree(source)
+        shutil.rmtree(dest)
+
+        
+
+    def test_when_a_file_exists_in_the_source_but_not_the_destination():
+        source_hashes = {'hash1': 'fn1'}
+        dest_hashes = {}
+        expected_actions = [('COPY', '/src/fn1', '/dst/fn1')]
